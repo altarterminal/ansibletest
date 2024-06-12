bc11a823129f8357d15ea24f51da2d8d105da1e9
altarterminal <118420278+nm-jp@users.noreply.github.com>
2024-06-13 00:36:34 +0900

    add playbook part

--- miniserver/run.sh
+++ miniserver/run.sh
@@ -14,4 +14,16 @@ PLAYBOOK_DIR='playbook'
 #####################################################################
 
 ${SCRIPT_DIR}/make_book.sh -d${PLAYBOOK_DIR} ${LEDGER_FILE}
-${SCRIPT_DIR}/make_inventory.sh  ${LEDGER_FILE} > inventory.ini
+${SCRIPT_DIR}/make_inventory.sh ${LEDGER_FILE} > inventory.ini
+
+#####################################################################
+# exec playbook
+#####################################################################
+
+find ${PLAYBOOK_DIR} -name "*.yml"                                  |
+
+while read -r playbook
+do
+  ansible-playbook -i inventory.ini ${playbook}                     |
+  ${SCRIPT_DIR}/parse_result.sh
+done
