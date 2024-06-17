434b517b7d2674c958977c86cffc595589963bb0
altarterminal <118420278+nm-jp@users.noreply.github.com>
2024-06-17 16:49:47 +0900

    add apt update task

--- miniserver/run.sh
+++ miniserver/run.sh
@@ -6,7 +6,8 @@ set -eu
 #####################################################################
 
 SCRIPT_DIR='script'
-LEDGER_FILE='ledger.json'
+TEMPLATE_DIR='template'
+LEDGER_FILE='test/ledger.json'
 PLAYBOOK_DIR='playbook'
 
 #####################################################################
@@ -14,14 +15,16 @@ PLAYBOOK_DIR='playbook'
 #####################################################################
 
 ${SCRIPT_DIR}/make_book.sh -d${PLAYBOOK_DIR} ${LEDGER_FILE}
-${SCRIPT_DIR}/make_inventory.sh ${LEDGER_FILE} > inventory.ini
+${SCRIPT_DIR}/make_inventory.sh ${LEDGER_FILE} > 'inventory.ini'
+
+cp "${TEMPLATE_DIR}/update_template.yml" "${PLAYBOOK_DIR}/playbook__update.yml"
 
 #####################################################################
 # exec playbook
 #####################################################################
 
 find ${PLAYBOOK_DIR} -name "*.yml"                                  |
-
+sort                                                                |
 while read -r playbook
 do
   ansible-playbook -i inventory.ini ${playbook}                     |
