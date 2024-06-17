63a6c25e6da4537c4bbed5c9d9fff998b9c0f870
altarterminal <118420278+nm-jp@users.noreply.github.com>
2024-06-18 00:29:37 +0900

    improve the quality of run script

--- miniserver/run.sh
+++ miniserver/run.sh
@@ -2,13 +2,52 @@
 set -eu
 
 #####################################################################
-# setting
+# help
 #####################################################################
 
-SCRIPT_DIR='script'
-TEMPLATE_DIR='template'
-LEDGER_FILE='test/ledger.json'
-PLAYBOOK_DIR='playbook'
+print_usage_and_exit () {
+  cat <<-USAGE 1>&2
+Usage   : ${0##*/} <ledger>
+Options :
+
+run maintenance process from <ledger>.
+USAGE
+  exit 1
+}
+
+#####################################################################
+# parameter
+#####################################################################
+
+opr=''
+
+i=1
+for arg in ${1+"$@"}
+do
+  case "$arg" in
+    -h|--help|--version) print_usage_and_exit ;;
+    *)
+      if [ $i -eq $# ] && [ -z "$opr" ]; then
+        opr=$arg
+      else
+        echo "${0##*/}: invalid args" 1>&2
+        exit 1
+      fi
+      ;;
+  esac
+
+  i=$((i + 1))
+done
+
+if [ ! -f "${opr}" ] || [ ! -r "${opr}" ]; then
+  echo "${0##*/}: <${opr}> cannot be accessed" 1>&2
+  exit 1
+fi
+
+readonly LEDGER_FILE=${opr}
+readonly SCRIPT_DIR='script'
+readonly TEMPLATE_DIR='template'
+readonly PLAYBOOK_DIR='playbook'
 
 #####################################################################
 # make files
