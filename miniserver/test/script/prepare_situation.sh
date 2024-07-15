dbeaa607eda73ec07115682eac3d71a8a0836d19
altarterminal <118420278+nm-jp@users.noreply.github.com>
2024-07-15 22:57:25 +0900

    catch up the latest IF

--- miniserver/test/script/prepare_situation.sh
+++ miniserver/test/script/prepare_situation.sh
@@ -11,12 +11,20 @@ TEST_DIR="${THIS_DIR}/.."
 SCRIPT_DIR="${TOP_DIR}/script"
 PLAYBOOK_DIR="${TEST_DIR}/pplaybook"
 
-LEDGER_FILE="${TEST_DIR}/ledger.json"
+SOFT_LEDGER_FILE="${TEST_DIR}/soft_ledger.json"
+HOST_LEDGER_FILE="${TEST_DIR}/host_ledger.json"
 INVENTORY_FILE="${TEST_DIR}/inventory.ini"
 PLAYBOOK_FILE="${PLAYBOOK_DIR}/prepare_situation.yml"
 
-${SCRIPT_DIR}/make_inventory.sh ${LEDGER_FILE} > "${INVENTORY_FILE}"
+${SCRIPT_DIR}/inventory.sh                                          |
+  -s"${SOFT_LEDGER_FILE}" "${HOST_LEDGER_FILE}"                     \
+  > "${INVENTORY_FILE}"
+
 cp "${TOP_DIR}/ansible.cfg" "${TEST_DIR}"
+
+#####################################################################
+# check the condition
+#####################################################################
 
 if ! docker ps | grep -q 'container-ansible-test0'; then
   echo "${0##*/}: some container not running" 1>&2
@@ -33,7 +41,7 @@ if ! docker ps | grep -q 'container-ansible-test2'; then
   exit 1
 fi
 
-if ! type ansible >/dev/null 2>&1; then
+if ! type ansible-playbook >/dev/null 2>&1; then
   echo "${0##*/}: ansible command not found" 1>&2
   exit 1
 fi
