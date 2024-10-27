9bf8c066b0200203a43b8351ecc00b3fd2da7c79
altarterminal <118420278+nm-jp@users.noreply.github.com>
2024-10-27 23:13:53 +0900

    improve script

--- pcsetup/script/setup_myaccount.sh
+++ pcsetup/script/setup_myaccount.sh
@@ -35,7 +35,7 @@ opt_d='no'
 i=1
 for arg in ${1+"$@"}
 do
-  case "$arg" in
+  case "${arg}" in
     -h|--help|--version) print_usage_and_exit ;;
     -k*)                 opt_k=${arg#-k}      ;;
     -u*)                 opt_u=${arg#-u}      ;;
@@ -43,10 +43,10 @@ do
     -p)                  opt_p='yes'          ;;
     -d)                  opt_d='yes'          ;;
     *)
-      if [ $i -eq $# ] && [ -z "$opr" ]; then
+      if [ $i -eq $# ] && [ -z "${opr}" ]; then
         opr=$arg
       else
-        echo "${0##*/}: invalid args" 1>&2
+        echo "ERROR:${0##*/}: invalid args" 1>&2
         exit 1
       fi
       ;;
@@ -56,28 +56,28 @@ do
 done
 
 if ! type ansible-playbook >/dev/null 2>&1; then
-  echo "${0##*/}: ansible command not found" 1>&2
+  echo "ERROR:${0##*/}: ansible command not found" 1>&2
   exit 1
 fi
 
 if [ ! -f "${opr}" ] || [ ! -r "${opr}" ]; then
-  echo "${0##*/}: invalid inventory <${opr}> specified" 1>&2
+  echo "ERROR:${0##*/}: invalid inventory specified <${opr}>" 1>&2
   exit 1
 fi
 
 if [ ! -f "${opt_k%.pub}" ]     || [ ! -r "${opt_k%.pub}" ] ||
    [ ! -f "${opt_k%.pub}.pub" ] || [ ! -r "${opt_k%.pub}.pub" ]; then
-  echo "${0##*/}: invalid key <${opt_k}> specified" 1>&2
+  echo "ERROR:${0##*/}: invalid key specified <${opt_k}>" 1>&2
   exit 1
 fi
 
 if [ -z "${opt_u}" ]; then
-  echo "${0##*/}: user name must be specified" 1>&2
+  echo "ERROR:${0##*/}: user name must be specified" 1>&2
   exit 1
 fi
 
 if ! printf '%s\n' "${opt_i}" | grep -Eq '^[0-9]+$'; then
-  echo "${0##*/}: invalid id <${opt_i}> specified" 1>&2
+  echo "${0##*/}: invalid id specified <${opt_i}>" 1>&2
   exit 1
 fi
 
