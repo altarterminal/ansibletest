a6d5d5db62980a8a054b292a5cc712832cccbc3f
altarterminal <118420278+nm-jp@users.noreply.github.com>
2025-01-15 14:37:14 +0900

    add a script for file update

---
+++ mysetup/file_update.sh
@@ -0,0 +1,175 @@
+#!/bin/sh
+set -eu
+
+#####################################################################
+# help
+#####################################################################
+
+print_usage_and_exit () {
+  cat <<-USAGE 1>&2
+Usage   : ${0##*/} <target file> <inventory file>
+Options : -u<user name> -c<update cotent> -f<update file> -d
+
+Update the file of <file path>
+
+-u: Specify the user name to manipulate (default: $(whoami)).
+-c: Specify the file content to overwrite (default: <<empty>>).
+-f: Specify the file path which includes the content to overwrite (This is prior to -c option if something is specified).
+-d: Enable dry-run (= only output the playbook and not execute it) (default: disabled).
+USAGE
+  exit 1
+}
+
+#####################################################################
+# parameter
+#####################################################################
+
+opr_f=''
+opr_i=''
+opt_u=$(whoami)
+opt_c=''
+opt_f=''
+opt_d='no'
+
+i=1
+for arg in ${1+"$@"}
+do
+  case "${arg}" in
+    -h|--help|--version) print_usage_and_exit ;;
+    -u*)                 opt_u=${arg#-u}      ;;
+    -c*)                 opt_c=${arg#-c}      ;;
+    -f*)                 opt_f=${arg#-f}      ;;
+    -d)                  opt_d='yes'          ;;
+    *)
+      if [ $i -eq $(($# - 1)) ] && [ -z "${opr_f}" ]; then
+        opr_f=${arg}
+      elif [ $i -eq $# ] && [ -z "${opr_i}" ]; then
+        opr_i=${arg}
+      else
+        echo "ERROR:${0##*/}: invalid args" 1>&2
+        exit 1
+      fi
+      ;;
+  esac
+
+  i=$((i + 1))
+done
+
+readonly IS_DRYRUN="${opt_d}"
+
+if [ "${IS_DRYRUN}" = 'no' ]; then
+  if ! type ansible-playbook >/dev/null 2>&1; then
+    echo "ERROR:${0##*/}: ansible command not found" 1>&2
+    exit 1
+  fi
+
+  if [ ! -f "${opr_i}" ] || [ ! -r "${opr_i}" ]; then
+    echo "ERROR:${0##*/}: invalid inventory specified <${opr_i}>" 1>&2
+    exit 1
+  fi
+
+  readonly INVENTORY="${opr_i}"
+fi
+
+if [ -z "${opt_u}" ]; then
+  echo "ERROR:${0##*/}: user name must be specified <${opt_u}>" 1>&2
+  exit 1
+fi
+
+if [ -n "${opt_f}" ]; then
+  if [ ! -f "${opt_f}" ] || [ ! -r "${opt_f}" ]; then
+    echo "ERROR:${0##*/}: invalid file specified" 1>&2
+    exit 1
+  fi
+
+  readonly IS_FILE='yes'
+  readonly INPUT_FILE="${opt_f}"
+else
+  readonly IS_FILE='no'
+  readonly INPUT_CONTENT=${opt_c}
+fi
+
+readonly TARGET_FILE="${opr_f}"
+readonly INVENTORY_FILE="${opr_i}"
+readonly USER_NAME="${opt_u}"
+
+readonly TEMP_PLAYBOOK_NAME="${TMPDIR:-/tmp}/${0##*/}_$(date '+%Y%m%d_%H%M%S')_playbook_XXXXXX"
+readonly TEMP_CONTENT_NAME="${TMPDIR:-/tmp}/${0##*/}_$(date '+%Y%m%d_%H%M%S')_content_XXXXXX"
+
+#####################################################################
+# prepare
+#####################################################################
+
+readonly PLAYBOOK_FILE=$(mktemp "${TEMP_PLAYBOOK_NAME}")
+readonly CONTENT_FILE=$(mktemp "${TEMP_CONTENT_NAME}")
+
+trap "
+  [ -e ${PLAYBOOK_FILE} ] && rm ${PLAYBOOK_FILE}
+  [ -e ${CONTENT_FILE} ] && rm ${CONTENT_FILE}
+" EXIT
+
+if [ "${IS_FILE}" = 'yes' ]; then
+  cat "${INPUT_FILE}" >"${CONTENT_FILE}"
+else
+  printf '%s\n' "${INPUT_CONTENT}" >"${CONTENT_FILE}"
+fi
+
+#####################################################################
+# main routine
+#####################################################################
+
+{
+  cat <<'EOF'                                                       |
+- name: update file
+  hosts: all
+  gather_facts: no
+  become: yes
+  become_user: <<user_name>>
+  vars:
+    user_name: <<user_name>>
+  tasks:
+    - name: check the account exist
+      ansible.builtin.shell: |
+        id "{{ user_name }}"
+      register: result
+      failed_when: result.rc not in [0, 1]
+
+    - name: exec update
+      when: result.rc == 0
+      block:
+        - name: get target path
+          ansible.builtin.shell:
+            echo "<<target_file>>"
+          register: get_result
+          become_user: "{{ user_name }}"
+
+        - name: set parameter
+          ansible.builtin.set_fact:
+            target_file: "{{ get_result.stdout }}"
+
+        - name: setup hosts
+          ansible.builtin.blockinfile:
+            path: "{{ target_file }}"
+            create: true
+            block: |
+              <<input_content>>
+          become_user: "{{ user_name }}" 
+EOF
+  sed 's#<<user_name>>#'"${USER_NAME}"'#'                           |
+  sed 's#<<target_file>>#'"${TARGET_FILE}"'#'                       |
+  awk '
+    /<<input_content>>/ {
+      while ((getline < "'"${CONTENT_FILE}"'") > 0) {
+        printf "              %s\n", $0
+      }
+      next
+    }
+    { print }
+  '
+} >"${PLAYBOOK_FILE}"
+
+if [ "${IS_DRYRUN}" = 'yes' ]; then
+  cat "${PLAYBOOK_FILE}"
+else
+  ANSIBLE_PIPELINING=1 ansible-playbook -i "${INVENTORY_FILE}" "${PLAYBOOK_FILE}"
+fi
