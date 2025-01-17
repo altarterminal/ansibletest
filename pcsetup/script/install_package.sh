a30200ed3005f08cbf75247f6efe4f287b98a17d
altarterminal <118420278+nm-jp@users.noreply.github.com>
2025-01-18 01:06:30 +0900

    improve

--- pcsetup/script/install_package.sh
+++ pcsetup/script/install_package.sh
@@ -7,13 +7,14 @@ set -eu
 
 print_usage_and_exit () {
   cat <<-USAGE 1>&2
-Usage   : ${0##*/} -i<inventory> <package ledger>
-Options : -d
+Usage   : ${0##*/} <inventory file>
+Options : -p<package list> -j<package ledger> -d
 
 Create accouts on <accout ledger> to hosts on <inventory>.
 If there has already been the account, nothing is done.
 
--d: enable dry run (default: no)
+-p: Specify the package comma-seperated list (default: 'openssh-client').
+-d: enable dry run (default: disabled).
 USAGE
   exit 1
 }
@@ -23,7 +24,8 @@ USAGE
 #####################################################################
 
 opr=''
-opt_i=''
+opt_p='openssh-client'
+opt_j=''
 opt_d='no'
 
 i=1
@@ -31,7 +33,8 @@ for arg in ${1+"$@"}
 do
   case "${arg}" in
     -h|--help|--version) print_usage_and_exit ;;
-    -i*)                 opt_i=${arg#-i}      ;;
+    -p*)                 opt_p=${arg#-p}      ;;
+    -j*)                 opt_j=${arg#-j}      ;;
     -d)                  opt_d='yes'          ;;
     *)
       if [ $i -eq $# ] && [ -z "${opr}" ]; then
@@ -54,39 +57,65 @@ if [ "${IS_DRYRUN}" = 'no' ]; then
     exit 1
   fi
 
-  if [ ! -f "${opt_i}" ] || [ ! -r "${opt_i}" ]; then
-    echo "ERROR:${0##*/}: invalid inventory specified <${opt_i}>" 1>&2
+  if [ ! -f "${opr}" ] || [ ! -r "${opr}" ]; then
+    echo "ERROR:${0##*/}: invalid inventory specified <${opr}>" 1>&2
     exit 1
   fi
 
-  readonly INVENTORY_FILE=${opt_i}
+  readonly INVENTORY_FILE="${opr}"
 fi
 
-if [ ! -f "${opr}" ] || [ ! -r "${opr}" ]; then
-  echo "ERROR:${0##*/}: invalid ledger specified <${opr}>" 1>&2
-  exit 1
+if [ -n "${opt_j}" ]; then
+  if [ ! -f "${opt_j}" ] || [ ! -r "${opt_j}" ]; then
+    echo "ERROR:${0##*/}: invalid file specified <${opt_j}>" 1>&2
+    exit 1
+  fi
+
+  readonly IS_JSON='yes'
+  readonly INPUT_JSON_FILE="${opt_j}"
+else
+  readonly IS_JSON='no'
+  readonly PACKAGE_LIST="${opt_p}"
 fi
 
-readonly LEDGER_FILE=${opr}
-readonly DATE=$(date '+%Y%m%d_%H%M%S')
+readonly DATE="$(date '+%Y%m%d_%H%M%S')"
 
-readonly TEMP_NAME=${TMPDIR:-/tmp}/${0##*/}_${DATE}_XXXXXX
+readonly TEMP_PLAYBOOK_NAME="${TMPDIR:-/tmp}/${0##*/}_${DATE}_playbook_XXXXXX"
+readonly TEMP_JSON_NAME="${TMPDIR:-/tmp}/${0##*/}_${DATE}_json_XXXXXX"
 
 #####################################################################
 # prepare
 #####################################################################
 
-readonly PLAYBOOK_FILE=$(mktemp "${TEMP_NAME}")
+readonly PLAYBOOK_FILE="$(mktemp "${TEMP_PLAYBOOK_NAME}")"
+readonly JSON_FILE="$(mktemp "${TEMP_JSON_NAME}")"
+
 trap "
   [ -e ${PLAYBOOK_FILE} ] && rm ${PLAYBOOK_FILE}
+  [ -e ${JSON_FILE} ] && rm ${JSON_FILE}
 " EXIT
+
+if [ "${IS_JSON}" = 'yes' ]; then
+  cat "${INPUT_JSON_FILE}" >"${JSON_FILE}"
+else
+  echo "${PACKAGE_LIST}"                                            |
+  tr ',' '\n'                                                       |
+  sed 's!=! !'                                                      |
+  while read -r name ver
+  do
+    printf '{"name":"%s","ver":"%s"},\n' "${name}" "${ver}"
+  done                                                              |
+  sed '$s!,$!!'                                                     |
+  { echo "["; cat; echo "]"; }                                      |
+  cat >"${JSON_FILE}"
+fi
 
 #####################################################################
 # main routine
 #####################################################################
 
 {
-  cat <<'EOF'
+  cat <<'  EOF'
 - name: install apt package
   hosts: all
   gather_facts: no
@@ -96,9 +125,9 @@ trap "
     ansible.builtin.apt:
       update_cache: yes
       name:
-EOF
+  EOF
 
-  jq -c '.[]' "${LEDGER_FILE}"                                      |
+  jq -c '.[]' "${JSON_FILE}"                                        |
   while read -r line;
   do
     package_name=$(echo "${line}" | jq -r '.name')
