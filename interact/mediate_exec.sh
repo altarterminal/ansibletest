4d65f2388c9de7a4acfcbceb5eb602053ec7bb62
altarterminal <118420278+nm-jp@users.noreply.github.com>
2025-02-02 17:03:56 +0900

    add output options to mediator

--- interact/mediate_exec.sh
+++ interact/mediate_exec.sh
@@ -52,13 +52,48 @@ readonly THIS_DIR=$(dirname $0)
 # main routine
 #####################################################################
 
+stdout_opt=''
+stderr_opt=''
+rtcode_opt=''
+
 while true
 do
   printf 'execute command ("q" for quit) > '
   read -r cmd
 
   case "${cmd}" in
-    q) exit 0 ;;
-    *) "${THIS_DIR}/exec_command.sh" "${INVENTORY_FILE}" "${cmd}" ;;
+    q)
+      exit 0 ;;
+    o|'o *')
+      if [ -z "${stdout_opt}" ]; then
+        stdout_opt='-so'
+        echo "Enabled standard out"
+      else
+        stdout_opt=''
+        echo "Disabled standard out"
+      fi
+      ;;
+    e|'e *')
+      if [ -z "${stderr_opt}" ]; then
+        stderr_opt='-se'
+        echo "Enabled standard error"
+      else
+        stderr_opt=''
+        echo "Disabled standard error"
+      fi
+      ;;
+    c|'c *')
+      if [ -z "${rtcode_opt}" ]; then
+        rtcode_opt='-rc'
+        echo "Enabled return code"
+      else
+        rtcode_opt=''
+        echo "Disabled return code"
+      fi
+      ;;
+    *)
+      "${THIS_DIR}/exec_command.sh"                                 \
+        ${stdout_opt} ${stderr_opt} ${rtcode_opt}                   \
+        "${INVENTORY_FILE}" "${cmd}" ;;
   esac
 done
