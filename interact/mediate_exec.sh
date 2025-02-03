209f9ead8bf4d6a13b11a9cccb67feb2a0417841
altarterminal <118420278+nm-jp@users.noreply.github.com>
2025-02-03 23:55:16 +0900

    update command

--- interact/mediate_exec.sh
+++ interact/mediate_exec.sh
@@ -48,76 +48,126 @@ readonly INVENTORY_FILE="${opr}"
 
 readonly THIS_DIR="$(dirname "$0")"
 
+readonly NOW_DATE="$(date '+%Y%m%d%H%M%S')"
+readonly PREV_RESULT_NAME="${TMPDIR:-/tmp}/${0##*/}_${NOW_DATE}_XXXXXX"
+
+#####################################################################
+# prepare
+#####################################################################
+
+readonly PREV_RESULT_FILE="$(mktemp "${PREV_RESULT_NAME}")"
+
+trap "
+  [ -e ${PREV_RESULT_FILE} ] && rm ${PREV_RESULT_FILE}
+" EXIT
+
 #####################################################################
 # utility
 #####################################################################
 
-print_state() (
-  if [ -z "${stdout_opt}" ]; then is_stdout=' No'; else is_stdout='Yes'; fi
-  if [ -z "${stderr_opt}" ]; then is_stderr=' No'; else is_stderr='Yes'; fi
-  if [ -z "${rtcode_opt}" ]; then is_rtcode=' No'; else is_rtcode='Yes'; fi
+is_stdout='yes'
+is_stderr='yes'
+is_rtcode='yes'
 
+print_state() (
 cat<<EOF
 #####################################################################
-Standard Out: [${is_stdout}] (to toggle please enter [o])
-Standard Err: [${is_stderr}] (to toggle please enter [e])
-Return Code:  [${is_rtcode}] (to toggle please enter [c])
+Standard Out: ${is_stdout}
+Standard Err: ${is_stderr}
+Return Code:  ${is_rtcode}
+---------------------------------------------------------------------
+h: Print this message.
+o: Switch the output of standard out.
+e: Switch the output of standart error.
+c: Switch the output of return code.
+w: Write the previous result to a specified file.
+q: Quit.
 #####################################################################
 EOF
 )
 
+exec_command() (
+  readonly CMD="$1"
+
+  if [ "${is_stdout}${is_stderr}${is_rtcode}" = 'nonono' ]; then
+    echo "Please enable at least one output"
+    return
+  fi
+
+  if [ "${is_stdout}" = 'yes' ]; then stdout_opt='-so'; else stdout_opt=''; fi
+  if [ "${is_stderr}" = 'yes' ]; then stderr_opt='-se'; else stderr_opt=''; fi
+  if [ "${is_rtcode}" = 'yes' ]; then rtcode_opt='-rc'; else rtcode_opt=''; fi
+
+  "${THIS_DIR}/exec_command.sh"                                     \
+    ${stdout_opt} ${stderr_opt} ${rtcode_opt}                       \
+   "${INVENTORY_FILE}" "${CMD}"                                     |
+
+   tee "${PREV_RESULT_FILE}"
+)
+
+output_file() (
+  readonly CMD="$1"
+
+  output_path=$(echo "${CMD}" | sed 's!^w  *!!')
+  output_file=$(basename "${output_path}")
+  output_dir=$(dirname "${output_path}")
+
+  if ! mkdir -p "${output_dir}"; then
+    echo "Failed to make a directory <${output_dir}>, so cannot write the result."
+    return
+  fi
+
+  if [ -e "${output_path}" ]; then
+    echo "The file of same name already exist <${output_file}>, specify another name."
+    return
+  fi
+
+  cp "${PREV_RESULT_FILE}" "${output_path}"
+)
 #####################################################################
 # main routine
 #####################################################################
 
-stdout_opt=''
-stderr_opt=''
-rtcode_opt=''
+print_state
 
 while true
 do
-  print_state
-  printf 'execute command ("q" for quit) > '
+  printf 'command ("h" for help) > '
   read -r cmd
 
   case "${cmd}" in
+    h)
+      print_state
+      ;;
     q)
-      exit 0 ;;
-    o|'o *')
-      if [ -z "${stdout_opt}" ]; then
-        stdout_opt='-so'
-        echo "Enabled standard out"
+      exit 0
+      ;;
+    o|o\ *)
+      if [ "${is_stdout}" = 'no' ]; then
+        is_stdout='yes'; echo "Enabled standard out"
       else
-        stdout_opt=''
-        echo "Disabled standard out"
+        is_stdout='no';  echo "Disabled standard out"
       fi
       ;;
-    e|'e *')
-      if [ -z "${stderr_opt}" ]; then
-        stderr_opt='-se'
-        echo "Enabled standard error"
+    e|e\ *)
+      if [ "${is_stderr}" = 'no' ]; then
+        is_stderr='yes'; echo "Enabled standard error"
       else
-        stderr_opt=''
-        echo "Disabled standard error"
+        is_stderr='no';  echo "Disabled standard error"
       fi
       ;;
-    c|'c *')
-      if [ -z "${rtcode_opt}" ]; then
-        rtcode_opt='-rc'
-        echo "Enabled return code"
+    c|c\ *)
+      if [ "${is_rtcode}" = 'no' ]; then
+        is_rtcode='yes'; echo "Enabled return code"
       else
-        rtcode_opt=''
-        echo "Disabled return code"
+        is_rtcode='no';  echo "Disabled return code"
       fi
+      ;;
+    w\ *)
+      output_file "${cmd}"
       ;;
     *)
-      if [ -z "${stdout_opt}${stderr_opt}${rtcode_opt}" ]; then
-        echo "Please enable any output"
-      else
-        "${THIS_DIR}/exec_command.sh"                               \
-          ${stdout_opt} ${stderr_opt} ${rtcode_opt}                 \
-          "${INVENTORY_FILE}" "${cmd}"
-      fi
+      exec_command "${cmd}"
       ;;
   esac
 done
